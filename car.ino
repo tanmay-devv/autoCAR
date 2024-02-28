0ffe6e429d017f43540c3ef03bef7550dba783ad
tanmay-devv <tanmaysharma3141@gmail.com>
2024-02-28 13:31:12 +0530

    updates

---
+++ car.ino
@@ -0,0 +1,25 @@
+// initiallizations 
+
+int motorin = 9; //fornow
+
+
+
+
+
+
+void setup()
+{
+
+pinMode(motorin, OUTPUT);
+Serial.begin(9600);
+
+}
+
+void loop()
+{
+ // digitalWrite(motorin, HIGH);
+string command[];
+
+
+
+}
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <array>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	string command[0];
+	cin << command[0] << endl;
+
+return 0;
+}
